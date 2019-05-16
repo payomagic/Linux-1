43d7084978574550108749be03a5902043501432
payozon <48397819+payozon@users.noreply.github.com>
2019-05-16 22:47:48 +0200

    Update PingMajorHam.sh
    added steps and remove of the step-files

--- PingMajorHam.sh
+++ PingMajorHam.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
+# H&M Major Outage Country ping
+# 2019 dvorapa
+#
+
+if [[ ! -f ${HMSTORES} ]]; then
+   echo "Processing...! In case you run this script first time - re-run it again to ping sites properly!"
+   mkdir ~/.major
+   cinfo henmau- >> ~/.major/all-sites.txt
+fi
+
 #mkdir ~/.major
-#cinfo henmau- >> .major/all-sites.txt
+#cinfo henmau- >> ~/.major/all-sites.txt
 read -p "Enter country code ( us, kr, gb ): " country
-cat .major/all-sites.txt | grep henmau-$country >> .major/output2.txt && cat .major/output2.txt | grep Router >> .major/output3.txt && touch major-ping && chmod +x major-ping && /bin/awk '{ print $'1' }' .major/output3.txt >> major-ping && awk '{print "pping "$0" "}' major-ping >> list2ping && chmod +x list2ping && ./list2ping >> major-list
+echo
+echo "Please be patient..."
+cat ~/.major/all-sites.txt | grep henmau-$country >> ~/.major/output2.txt && cat ~/.major/output2.txt | grep Router >> ~/.major/output3.txt && touch major-ping && chmod +x major-ping && /bin/awk '{ print $'1' }' ~/.major/output3.txt >> major-ping && awk '{print "pping "$0" "}' major-ping >> ~/list2ping && chmod +x ~/list2ping && ~/list2ping >> ~/major-list
+echo "All sites in chosen country with Router status : "
+echo
+echo "===== Country Chosen : $country ====="
+cat ~/major-list
+echo 
+echo "====================================="
+rm ~/.major/output*
+rm ~/major-list
